4ccedd078c7437d20007b45687f49b509be02356
Akins <Akins@RONNIES-MAC-BOOK.local>
2020-01-16 11:27:17 -0800

    called to the API from js, developing fur. Dev further to display more json data

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,28 @@
-// add autocomplete for axios
-const axios = require('axios').default;
+// alert("Hiii from js")
+
 
 // add base url for searching API
-yardsAPI = '127.0.0.1:5000/APIApp/?';
+let yardsAPI = "http://127.0.0.1:5000/APIApp/?";
+let APISearchInput = document.getElementById("APISearch");
+let searchParamInput = document.getElementById("APISearchOptions");
+
+let scientificName = document.getElementById("scientificName");
 
 
-
+// Function used on profile to search API
 function searchAPI() {
-  axios
-    .get()
-    .then(res => showOutput(res))
-    .catch(err => console.error(err));
+  axios.get(yardsAPI + searchParamInput.value + APISearchInput.value)
+    .then(function(response){
+      searchData = response.data;
+      scientificName.innerHTML = searchData[0].scientificNameAuthor;
+      nationalCommonName.innerHTML = searchData[0].nationalCommonName;
+      family.innerHTML = searchData[0].family;
+    })
+    .catch(function (error) {
+      scientificName.innerHTML = "(An error has occurred.)"
+    });
 }
+
+let searchButton = document.getElementById("plantSearchButton");
+searchButton.addEventListener("click", searchAPI);
+
