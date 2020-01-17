82c7ca098efba438eb5ffb853d9607f462e300cf
Akins <Akins@Jamess-Air.localdomain>
2020-01-16 20:31:34 -0800

    commit before making userPlantList model

--- static/js/main.js
+++ static/js/main.js
@@ -14,6 +14,7 @@ function searchAPI() {
   axios.get(yardsAPI + searchParamInput.value + APISearchInput.value)
     .then(function(response){
       searchData = response.data;
+      console.log(searchData)
       scientificName.innerHTML = searchData[0].scientificNameAuthor;
       nationalCommonName.innerHTML = searchData[0].nationalCommonName;
       family.innerHTML = searchData[0].family;
@@ -26,3 +27,26 @@ function searchAPI() {
 let searchButton = document.getElementById("plantSearchButton");
 searchButton.addEventListener("click", searchAPI);
 
+// // get multiple items from rest
+// function returnAPIItems() {
+//   // length is the number of times for loop will repeat
+//   let length = 10
+//   let container = document.creat
+
+//   for (i=0; i<length; i++) {
+//     // create an item to hold json data
+
+
+//     // add data to created item
+//     axios.get(yardsAPI + searchParamInput.value + APISearchInput.value)
+//     .then(function(response){
+//       searchData = response.data;
+//       scientificName.innerHTML = searchData[i].scientificNameAuthor;
+//       nationalCommonName.innerHTML = searchData[i].nationalCommonName;
+//       family.innerHTML = searchData[i].family;
+//     })
+//     .catch(function (error) {
+//       scientificName.innerHTML = "(An error has occurred.)"
+//     });
+//   }
+// }
--- yardsApp/models.py
+++ yardsApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# class userPlantList(models.Model):
+#     user = models.OneToOneField(
+#         users.CustomUser,
+#         on_delete=models.CASCADE, 
+#         primary_key=True
+#     )
+#     # items from API
+#     symbol = models.CharField(max_length=15, blank=True, default='')
+#     synonymSymbol = models.CharField(max_length=15, blank=True, default='')
+#     scientificNameAuthor = models.CharField(max_length=500, blank=True, default='' )
+#     nationalCommonName = models.CharField(max_length=500, blank=True, default='')
+#     family = models.CharField(max_length=250, blank=True, default='')
+#     nativeState = models.CharField(blank=False, max_length=100)
