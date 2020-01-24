b8ea8589b26c2fb99b6e833c16043f28f9ad86c8
Akins <Akins@RONNIES-MAC-BOOK.local>
2020-01-24 14:19:36 -0800

    table  wrking, trying vue POST method w/ axios

--- static/js/profile.js
+++ static/js/profile.js
@@ -15,6 +15,7 @@ const appSearch = new Vue({
     data: {
         searchInput: "",
         searchResults: [],
+        // need to get data from for loop into addPlant
         // addPlant: {'scientificNameAuthor':null, 'nationalCommonName':null, 'family':null, 'nativeState':null, 'isInvasive':null}
     },
     mounted: function() {
@@ -29,16 +30,54 @@ const appSearch = new Vue({
             })
     },
     // methods:{
-    //     getPlantData : function() {
-    //         this.$http.post('/newUserPlant/', this.searchResult)
-    //             .then((response) {
+        // need to get individual item upon clicking the button
+        // function calls api with symbol
+
+        // attempt with Axios
+        // getPlant : function(symbol) {
+        //     axios.get('http://127.0.0.1:5000/APIApp/?symbol__iexact=' + symbol)
+        //     .then(response => {
+        //         plantInfo = response.data
+        //         postData = {'scientificNameAuthor': plantInfo.scientificNameAuthor, 'nationalCommonName': plantInfo.nationalCommonName, 'family': plantInfo.family, 'nativeState': plantInfo.nativeState, 'isInvasive': plantInfo.isInvasive}
+
+        //         // post data collected with axios
+        //         axios.post('newUserPlant', postData)
+        //             .then(response => {
+        //                 console.log(response);
+        //             })
+        //             .catch(error => {
+        //                 console.log(error);
+        //             })
+        //         })
+        //     .catch(error => {
+        //         console.log(error);
+        //     })
+        // }
 
 
-    //             })
-                
-    //     }
 
-    // },
+        // old attempt:
+        // getPlant : function(symbol) {
+        //     this.$http.get("http://127.0.0.1:5000/APIApp/?symbol__iexact=${symbol}")
+        //         .then(response => {
+        //             this.plant = response.data;
+        //             console.log(response);
+        //         })
+        //         .catch((err) => {
+        //             console.log(err);
+        //         })
+        // },
+
+        // newPlant : function() {
+        //     this.$http.post(url, this.addPlant)
+        //         .then((response) => {
+        //             this.getPlant();
+        //         })
+        //         .catch ((err) => {
+        //             console.log(err);
+        //         })
+        // }
+    // }
     computed:{
         filteredResults: function(){
             return this.searchResults.filter((searchResult) => {
