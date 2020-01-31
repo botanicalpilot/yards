1948a8f8b40db8695f5831746e2a9baa704112cf
Akins <Akins@RONNIES-MAC-BOOK.local>
2020-01-31 10:31:46 -0800

    customUserPlant modal working.

--- static/js/main.js
+++ static/js/main.js
@@ -41,32 +41,23 @@ axios.get(endpoint)
   })
 
 
-// popup modal for customUserPlant submission
-// Get the modal
-let modal = document.getElementById("customUserPlantModal");
+let modal = document.querySelector(".modal");
+let openButton = document.getElementById("customPlantBtn");
+let closeButton = document.querySelector(".close-button");
 
-// Get the button that opens the modal
-let customPlantBtn = document.getElementById("customPlantBtn");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-customPlantBtn.onclick = function() {
-  modal.style.display = "block";
+function toggleModal() {
+  modal.classList.toggle("show-modal");
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function WindowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
   }
-} 
+}
+
+openButton.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", WindowOnClick);
 
 
 
